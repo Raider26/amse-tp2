830a7853584bab3f199b715978b1579ec1030e14
Raider26 <guillaumefoissy@gmail.com>
2025-02-24 14:53:36 +0100

    Exo 1

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -10,11 +10,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -33,13 +28,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Image(image: NetworkImage("https://picsum.photos/512/1024")),
-      ),
+      body: Image(image: NetworkImage("https://picsum.photos/1024/512")),
     );
   }
 }
