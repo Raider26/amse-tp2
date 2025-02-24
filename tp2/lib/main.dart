21e055664920e3cbaed6ecbed4d7ad0b59653521
Raider26 <guillaumefoissy@gmail.com>
2025-02-24 16:45:46 +0100

    Exo3

--- tp2/lib/main.dart
+++ tp2/lib/main.dart
@@ -1,7 +1,5 @@
-import 'dart:math';
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:tp2/exo2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,144 +11,48 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  double zValue = 0;
-  double xValue = 0;
-  bool? mirror = false;
-  double scale = 1;
-  IconData startStop = Icons.play_arrow;
-  Timer? timer;
-
-  void animate(Timer t) {
-    setState(() {
-      zValue >= 2 * pi ? zValue = 0 : zValue = zValue + 0.01;
-      xValue >= 2 * pi ? xValue = 0 : xValue = xValue + 0.01;
-      scale >= 5 ? scale = 0 : scale = scale + 0.001;
-    });
-  }
+  final List<Map<String, dynamic>> items = [
+    {"title": "Exo 2", "page": Exo2()},
+    {"title": "Exo 4", "page": Exo2()},
+    {"title": "Exo 5", "page": Exo2()},
+    {"title": "Exo 6", "page": Exo2()},
+    {"title": "Exo 7", "page": Exo2()}
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Transform(
-            alignment: Alignment.center,
-            transform: Matrix4.identity()
-              ..rotateX(xValue)
-              ..rotateZ(zValue)
-              ..rotateY(mirror == false ? 0 : pi)
-              ..scale(scale),
-            child: Container(
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(color: Colors.white),
-                child: Image(
-                    image: NetworkImage("https://picsum.photos/1024/512")))),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Rotate Z: "),
-            SizedBox(
-              width: 300,
-              child: Slider(
-                  value: xValue,
-                  min: 0,
-                  max: 2 * pi,
-                  onChanged: (value) {
-                    setState(() {
-                      xValue = value;
-                    });
-                  }),
-            )
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Rotate X: "),
-            SizedBox(
-              width: 300,
-              child: Slider(
-                  value: zValue,
-                  min: 0,
-                  max: 2 * pi,
-                  onChanged: (value) {
-                    setState(() {
-                      zValue = value;
-                    });
-                  }),
-            )
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Mirror: "),
-            Checkbox(
-                value: mirror,
-                onChanged: (bool? value) {
-                  setState(() {
-                    mirror = value;
-                  });
-                })
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Scale: "),
-            SizedBox(
-              width: 300,
-              child: Slider(
-                  value: scale,
-                  min: 0,
-                  divisions: 20,
-                  max: 5,
-                  onChanged: (value) {
-                    setState(() {
-                      scale = value;
-                    });
-                  }),
-            )
-          ],
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            SizedBox(
-              width: 100,
-              child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    if (startStop == Icons.play_arrow) {
-                      startStop = Icons.pause;
-                      const d = Duration(milliseconds: 50);
-                      timer = Timer.periodic(d, animate);
-                    } else {
-                      startStop = Icons.play_arrow;
-                      timer?.cancel();
-                    }
-                  });
-                },
-                child: Icon(startStop),
-              ),
-            )
-          ],
-        ),
-      ]),
+      appBar: AppBar(title: Text("Liste des pages")),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]['title']),
+            leading: Icon(Icons.arrow_forward),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => items[index]['page'],
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
