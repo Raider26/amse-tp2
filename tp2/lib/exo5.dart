a0f7aedc94f2462183b9f64bb6245faa1efde0f8
Raider26 <guillaumefoissy@gmail.com>
2025-02-25 22:15:40 +0100

    Exercice 5

--- tp2/lib/exo5.dart
+++ tp2/lib/exo5.dart
@@ -1,5 +1,49 @@
 import 'package:flutter/material.dart';
 
+class Tile {
+  String imageURL;
+  Alignment alignment;
+
+  Tile({required this.imageURL, required this.alignment});
+
+  Widget croppedImageTile(int gridSize) {
+    double factor = 1 / gridSize;
+    return FittedBox(
+      fit: BoxFit.fill,
+      child: ClipRect(
+        child: Align(
+          alignment: alignment,
+          widthFactor: factor,
+          heightFactor: factor,
+          child: Image.network(imageURL),
+        ),
+      ),
+    );
+  }
+}
+
+Tile tile1 =
+    Tile(imageURL: 'https://picsum.photos/300', alignment: Alignment(-1, -1));
+
+Tile tile2 =
+    Tile(imageURL: 'https://picsum.photos/300', alignment: Alignment(0, -1));
+
+List<Widget> createTile(int gridSize) {
+  List<Widget> grid = [];
+  double step = 2 / (gridSize - 1);
+
+  for (int i = 0; i < gridSize * gridSize; i++) {
+    double x = -1 + (i % gridSize) * step;
+    double y = -1 + (i ~/ gridSize) * step;
+    grid.add(Container(
+        child: Tile(
+                imageURL: 'https://picsum.photos/1024',
+                alignment: Alignment(x, y))
+            .croppedImageTile(gridSize)));
+  }
+  return grid;
+}
+
 class Exo5 extends StatefulWidget {
   const Exo5({super.key});
 
@@ -8,97 +52,51 @@ class Exo5 extends StatefulWidget {
 }
 
 class _Exo5State extends State<Exo5> {
+  int gridSize = 3;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Exercice 5"),
-        ),
-        body: Center(
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height * 0.6,
-            child: GridView(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 1),
-              children: [
-                Container(
-                  color: Colors.blue,
-                  child: Center(
-                      child: Text(
-                    "1",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )),
-                ),
-                Container(
-                  color: Colors.purple,
-                  child: Center(
-                      child: Text(
-                    "2",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )),
-                ),
-                Container(
-                  color: Colors.green,
-                  child: Center(
-                      child: Text(
-                    "3",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )),
-                ),
-                Container(
-                  color: Colors.yellow,
-                  child: Center(
-                      child: Text(
-                    "4",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )),
-                ),
-                Container(
-                  color: Colors.orange,
-                  child: Center(
-                      child: Text(
-                    "5",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )),
-                ),
-                Container(
-                  color: Colors.red,
-                  child: Center(
-                      child: Text(
-                    "6",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )),
-                ),
-                Container(
-                  color: Colors.white,
-                  child: Center(
-                      child: Text(
-                    "7",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )),
-                ),
-                Container(
-                  color: Colors.grey,
-                  child: Center(
-                      child: Text(
-                    "8",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )),
-                ),
-                Container(
-                  color: Colors.brown,
-                  child: Center(
-                      child: Text(
-                    "9",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  )),
-                )
-              ],
+      appBar: AppBar(
+        title: Text("Exercice 5"),
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.6,
+              child: GridView(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: gridSize,
+                    crossAxisSpacing: 10,
+                    mainAxisSpacing: 10,
+                    childAspectRatio: 1),
+                children: createTile(gridSize),
+              ),
             ),
           ),
-        ));
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Size: "),
+              SizedBox(
+                width: 300,
+                child: Slider(
+                    value: gridSize.toDouble(),
+                    label: gridSize.toString(),
+                    min: 1,
+                    divisions: 10,
+                    max: 10,
+                    onChanged: (value) {
+                      setState(() {
+                        gridSize = value.toInt();
+                      });
+                    }),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
